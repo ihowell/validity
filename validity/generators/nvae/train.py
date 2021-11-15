d9adf52020da5f93f55e1248a57d5d3a316af192
Ian Howell <ianh413@gmail.com>
2021-11-14 20:36:38 -0800

    Updated init process to always work.

--- validity/generators/nvae/train.py
+++ validity/generators/nvae/train.py
@@ -336,9 +336,15 @@ def test_vae_fid(model, args, total_fid_samples):
 def init_processes(rank, size, fn, args):
     """ Initialize the distributed environment. """
     os.environ['MASTER_ADDR'] = args.master_address
-    os.environ['MASTER_PORT'] = '6020'
     torch.cuda.set_device(args.local_rank)
-    dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
+    for i in range(100):
+        os.environ['MASTER_PORT'] = str(6020 + i)
+        try:
+            dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
+            break
+        except RuntimeError as e:
+            if 'Address already in use' not in e.message:
+                raise
     fn(args)
     cleanup()
 
