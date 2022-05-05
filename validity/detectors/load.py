1cc0d5d8b0928930c96eca26e2d1313ff93b9316
Ian Howell <ianh413@gmail.com>
2022-05-05 10:51:06 -0700

    Test.

--- validity/detectors/load.py
+++ validity/detectors/load.py
@@ -47,13 +47,13 @@ def load_detectors(in_ds_name, out_ds_name, cls_type, adv_attack, adv_step=True,
     return ood_detectors, adv_detectors
 
 
-def load_ood_detectors(cls_type, in_ds_name, out_ds_name, id=None):
-    llr_ood = load_llr(in_ds_name, out_ds_name, 0.3, id=id)
-    odin_ood = load_best_odin(cls_type, in_ds_name, out_ds_name, id=id)
+def load_ood_detectors(cls_type, in_ds_name, out_ds_name, classifier_id=None):
+    llr_ood = load_llr(in_ds_name, out_ds_name, 0.3, id=classifier_id)
+    odin_ood = load_best_odin(cls_type, in_ds_name, out_ds_name, id=classifier_id)
     mahalanobis_ood = load_best_mahalanobis_ood(cls_type,
                                                 in_ds_name,
                                                 out_ds_name,
-                                                classifier_id=id)
+                                                classifier_id=classifier_id)
 
     ood_detectors = [
         (f'ODIN OOD {out_ds_name}', odin_ood),
@@ -63,12 +63,12 @@ def load_ood_detectors(cls_type, in_ds_name, out_ds_name, id=None):
     return ood_detectors
 
 
-def load_adv_detectors(cls_type, ds_name, adv_attack, id=None):
-    lid_adv = load_best_lid(cls_type, ds_name, adv_attack, id=id)
+def load_adv_detectors(cls_type, ds_name, adv_attack, classifier_id=None):
+    lid_adv = load_best_lid(cls_type, ds_name, adv_attack, id=classifier_id)
     mahalanobis_adv = load_best_mahalanobis_adv(cls_type,
                                                 ds_name,
                                                 adv_attack,
-                                                classifier_id=id)
+                                                classifier_id=classifier_id)
 
     adv_detectors = [
         (f'LID Adv {adv_attack}', lid_adv),
